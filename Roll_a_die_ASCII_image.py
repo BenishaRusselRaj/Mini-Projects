4086784ca5d93c1f1d25db7d42119cd7161ef576
Benisha <42401521+BenishaRusselRaj@users.noreply.github.com>
2024-08-27 15:59:29 +0530

    Rolling a single die

---
+++ Roll_a_die_ASCII_image.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 12:11:18 2024
+
+@author: IITM
+"""
+import random
+
+num_gen = random.randint(1,6)
+
+DICE_ART = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+DIE_HEIGHT = len(DICE_ART[1])
+die_width = len(DICE_ART[1][0])
+
+dice_faces_rows = []
+row_components = []
+
+for row_idx in range(DIE_HEIGHT):
+    dice_faces_rows.append(DICE_ART[num_gen][row_idx])
+
+#%%
+header = [" RESULTS ".center(die_width,'=')]
+header.extend(dice_faces_rows)
+
+die_val = '\n'.join(header)
+
+print(die_val)
+
