2a8300841800d7d2ca483882bec0ba26b49258e0
Benisha <42401521+BenishaRusselRaj@users.noreply.github.com>
2025-09-17 12:14:49 +0530

    Update dna_count.py

--- dna_count.py
+++ dna_count.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 15 16:31:29 2025
 
-@author: CBEEV
-"""
-
-f = open(r"C:\Users\CBEEV\Downloads\rosalind_dna (2).txt")
+f = open(r"*\rosalind_dna (2).txt")
 
 dna_data = f.read()
 
 for i in 'ACGT':
     val_count = dna_data.count(i)
+
     print (val_count)
