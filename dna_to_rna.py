1b4a145b527cdcdfb7f5654d46b079057768b829
Benisha <42401521+BenishaRusselRaj@users.noreply.github.com>
2025-09-17 12:15:07 +0530

    Update dna_to_rna.py

--- dna_to_rna.py
+++ dna_to_rna.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 15 17:36:15 2025
 
-@author: CBEEV
-"""
-
-f = open(r"C:\Users\CBEEV\Downloads\rosalind_rna.txt")
+f = open(r"*\rosalind_rna.txt")
 
 dna_data = f.read()
 
 rna_data = dna_data.replace('T', 'U')
+
 
 print(rna_data)
